b768c07c3c814d56ceb2cb0b21dc9ab9aef7d0f3
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-05 23:36:18 -0400

    add assets, display island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,34 @@
   POPUP
 </div> */}
 
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
+import Island from '../models/Island';
+
+
 
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      <Canvas>
-        
+      <Canvas 
+        className="w-full h-screen bg-transparent"
+        camera={{near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island />
+        </Suspense>
       </Canvas>
     </section>
   )
 }
 
 export default Home
+
+// need to credit: "Fox's islands" (https://skfb.ly/6XpAQ) by nimzu is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
