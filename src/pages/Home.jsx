3d0044d6bd7af54b18ca9ed95d46f5513196aa3d
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-05 22:45:53 -0400

    add react-three/fiber

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POPUP
+</div> */}
+
+import { Canvas } from '@react-three/fiber';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+      <Canvas>
+        
+      </Canvas>
+    </section>
   )
 }
 
