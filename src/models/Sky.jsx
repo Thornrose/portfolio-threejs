09b27317e86476b6c9c0ae850fdab9729b6b54c4
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-06 06:51:07 -0400

    add skybox

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei"
+
+import skyScene from "../assets/3d/sky.glb"
+
 const Sky = () => {
+  const sky = useGLTF(skyScene);
+
   return (
-    <div>Sky</div>
+    <mesh>
+      <primitive object={sky.scene} />
+    </mesh>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
+import Sky from '../models/Sky';
 
 
 
@@ -38,7 +39,7 @@ const Home = () => {
           <directionalLight position={[1, 1, 1]} intensity={2}/>
           <ambientLight intensity={0.5}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
-
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
