01d78a0e6652a634bbde84732a80f70ca5be892c
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-06 10:35:22 -0400

    fix useRef, add Stage states

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/no-unknown-property */
-import { useGLTF } from "@react-three/drei"
+import { useEffect, useRef } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei"
 
 import planeScene from "../assets/3d/plane.glb"
 
 const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+
+  }, [actions, isRotating]);
+
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   )
