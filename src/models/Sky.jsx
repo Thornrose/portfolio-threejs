46fa66661cb6a3f42521a2d12ff13c70b7ef8560
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-06 10:50:25 -0400

    add basic sky rotation

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/no-unknown-property */
-import { useGLTF } from "@react-three/drei"
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 
 import skyScene from "../assets/3d/sky.glb"
 
-const Sky = () => {
+// current setup does not lock sky rotation to island rotation / key presses
+
+const Sky = ({ isRotating }) => {
+  const skyRef = useRef();
   const sky = useGLTF(skyScene);
 
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta;
+    }
+  })
+
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       <primitive object={sky.scene} />
     </mesh>
   )
