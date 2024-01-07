e1df2457092d900f483e4c7fca9008641061d703
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-07 14:55:12 -0400

    add emailjs and form handling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,53 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({name: "", email: "", message: ""});
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {
-
+  const handleChange = (e) => {
+      setForm({ ...form, [e.target.name]: e.target.value })
   };
 
-  const handleFocus = () => {};
-  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Ames",
+        from_email: form.email,
+        to_email: import.meta.env.VITE_APP_EMAILJS_TO_EMAIL,
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      // TODO: Show / hide success alert
+
+      setForm({name: "", email: "", message: ""});
+    }).catch ((error) => {
+      setIsLoading(false);
+      console.log(error);
+      // TODO: Show / hide error message
+    })
+  };
+
+  const handleFocus = (e) => {};
+
+  const handleBlur = (e) => {};
+
+
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get In Touch</h1>
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
           <label className="text-black-500 font-semibold">
             Name
             <input 
@@ -77,4 +109,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
