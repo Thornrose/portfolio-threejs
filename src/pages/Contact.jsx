c2f578ed2ba0b99b89b3f0bf07e35c4eef27f668
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-07 14:04:53 -0400

    add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,79 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [form, setForm] = useState({name: "", email: "", message: ""});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = () => {
+
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
   return (
-    <div>Contact</div>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get In Touch</h1>
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input 
+              type="text" 
+              name="name" 
+              placeholder="enter your name" 
+              className="input" 
+              required 
+              value={form.name} 
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+              
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="enter your email address" 
+              className="input" 
+              required 
+              value={form.email} 
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+              
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea 
+              name="message" 
+              placeholder="..." 
+              className="textarea" 
+              rows={4}
+              required 
+              value={form.message} 
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+              
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
