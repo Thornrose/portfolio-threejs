57a29e6d1d58dee1c0d1acab495670b5b7f61839
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-13 15:36:57 -0400

    add sound and favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 
-import { Suspense, useState } from 'react';
+import { Suspense, useState, useEffect, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import HomeInfo from '../components/HomeInfo';
@@ -9,11 +9,30 @@ import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 
+import sakura from "../assets/sakura.mp3";
+import { soundoff, soundon } from '../assets/icons';
+
 
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
+
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  useEffect(() => {
+    if(isPlayingMusic) {
+      audioRef.current.play();
+    }
+
+    return () => {
+      audioRef.current.pause();
+    }
+  }, [isPlayingMusic])
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let  screenPosition = [0, -6.5, -43];
@@ -79,6 +98,14 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
+      <div className="absolute bottom-2 left-2">
+        <img 
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="W-10 h-10 cursor-pointer object-contain"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   )
 }
