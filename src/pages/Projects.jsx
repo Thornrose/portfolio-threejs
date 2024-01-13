969d9cec2b50038a9d0ac2c0a71d442379b93267
Cheever Esler <108239850+Thornrose@users.noreply.github.com>
2024-01-13 10:50:49 -0400

    initial formatting for Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,22 @@
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-container">
+      <h1 className="head-text">
+        My <span className="blue-gradient_text font-semibold drop-shadow">Projects</span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Here are some of my most recent projects:
+        </p>
+      </div>
+
+      <div className="flex flex-wrap my-20 gap-16">
+        
+
+      </div>
+    </section>
   )
 }
 
